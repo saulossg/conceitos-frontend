dc426538da6530fa1bba6a0aeb1bea7e6e9bb785
Saulo Gomes <saulo.gomes@tbfg.com.br>
2020-05-06 23:08:40 -0300

    add repositories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]); 
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('repositories', {
+      title: `Repositório: ${ Date.now() }`,
+      url: "http://github.com.br/users/saulossg",
+      techs: ["NodeJS", "ReactJS", "React-Native"]
+    })
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +27,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
+      {repositories.map(repository => 
+        <li key={repository.title}>
+          { repository.title }
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
         </li>
+      )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
